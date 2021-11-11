ee200707795d3aefec0ef9272e0ff176a5965810
CristhianArcos <85269995+CristhianArcos@users.noreply.github.com>
2021-11-11 17:05:47 -0600

    Add files via upload

---
+++ CodigoC.cpp
@@ -0,0 +1,34 @@
+#Textos
+puts "Hola, mundo!"
+puts ""
+puts "Me gusta" + "el pastel de manzana."
+puts "Mi IP es: 192.168.9.10"
+puts "ragustin726@gmail.com"
+puts "https://www.facebook.com"
+
+#Variables
+myString = '...puedes decir eso de nuevo...'
+puts myString
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+ID= 10.0.0.1
+correo='roberto_840@hotmail.com'
+direccion='https://www.google.com'
+fecha = '19/07/2001'
+
+#Concatenaciones
+puts 'Me llamo ' + name + '.'
+puts 'Wow!  "' + name + '" es un nombre realmente largo!'
+composer1 = 'Mozart'
+puts composer1 + ' fue "el amo", en su día.'
+composer = 'Beethoven'
+puts 'Pero yo prefiero a ' + composer1 + ', personalmente.'
+
+
+#Condicionales
+puts 1.7 > 2
+puts 1 < 2
+puts 5 >= 5
+puts 5 <= 4
+puts 1 == 1
+puts 2 != 1
+puts 'gato' < 'perro'
